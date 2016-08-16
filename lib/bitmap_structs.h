1750a9ad5ec53a30b38b0882ab5dd04a29f46879
Richard Merry <rmerry@gmail.com>
2016-08-17 00:13:00 +0100

    added new bitmap_structs.h file

---
+++ lib/bitmap_structs.h
@@ -0,0 +1,47 @@
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#include <stdlib.h>
+
+/*
+ * Platforms: Windows 2.0+ / OS/2 1.x
+ * Notes: These bitmaps are uncompressed and do not support
+ *        bits per pixel values of 16 or 32
+ */
+typedef struct BITMAPCOREHEADER
+{
+  uint16_t width;
+  uint16_t height;
+  uint16_t colour_planes; /* Must be 1 */
+  uint16_t bits_per_pixel; /* Color depth; i.e., 1,2,4,8,16,24,32 */
+} BITMAPCOREHEADER;
+
+/*
+ *
+ *
+ */
+typedef struct BITMAPINFOHEADER
+{
+  int32_t width;
+  int32_t height;
+  uint16_t colour_planes; /* Must be 1 */
+  uint16_t bits_per_pixel; /* Color depth; i.e., 1,2,4,8,16,24,32 */
+  uint32_t compression_method; /* This can be 0 for BI_RGB bitmap types */
+  uint32_t raw_pixel_data_size_bytes;
+  int32_t horizonal_resolution; /* Pixels per meter */
+  int32_t vertical_resolution; /* Pixels per meter */
+  int32_t colours_in_palette; /* 0 to default to 2^n */
+  int32_t important_colours; /* 0 to indicate all colours are important */
+} BITMAPINFOHEADER;
+
